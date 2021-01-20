8f6eda930ca4bd29434f8625977a5c3f5aa55317
Zack Hickman <hickmanzackary@gmail.com>
2021-01-20 00:48:53 -0600

    where did all that fucking javascript come from

--- src/plugins/vuetify.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var vue_1 = require("vue");
-var framework_1 = require("vuetify/lib/framework");
-vue_1["default"].use(framework_1["default"]);
-exports["default"] = new framework_1["default"]({
-    theme: { dark: false }
-});
